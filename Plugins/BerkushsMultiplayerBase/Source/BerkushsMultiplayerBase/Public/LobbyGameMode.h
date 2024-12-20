c808eb6f4e080bc31e32d38555085de480c9146e
berkush-eudemonia <remziberk.sozgen@eudemoniagames.com>
2024-12-20 22:32:43 +0300

    Plugins reintegrated without cache files

---
+++ Plugins/BerkushsMultiplayerBase/Source/BerkushsMultiplayerBase/Public/LobbyGameMode.h
@@ -0,0 +1,33 @@
+// 2024'te R. Berk Sozgen tarafindan gelistirilmeye baslanmistir.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MultiplayerGameMode.h"
+#include "LobbyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BERKUSHSMULTIPLAYERBASE_API ALobbyGameMode : public AMultiplayerGameMode
+{
+	GENERATED_BODY()
+
+public:
+
+	void PostLogin(APlayerController* NewPlayer) override;
+
+	void Logout(AController* Exiting) override;
+
+	UFUNCTION(BlueprintCallable)
+	void StartGame(FString MapPath);
+	
+private:
+
+	FTimerHandle GameStartTimer;
+
+	uint32 NumberOfPlayers = 0;
+	
+};
+//push comment
