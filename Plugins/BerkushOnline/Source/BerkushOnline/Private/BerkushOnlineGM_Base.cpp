ed88a0ae28d1b8477a24d71ac08f9a1b287b7485
berkush-eudemonia <remziberk.sozgen@eudemoniagames.com>
2024-12-29 22:03:25 +0300

    Fixing Customization Build Error, And Created Another gamemode for making more dependant from OldMPBase

---
+++ Plugins/BerkushOnline/Source/BerkushOnline/Private/BerkushOnlineGM_Base.cpp
@@ -0,0 +1,5 @@
+// 2024'te R. Berk Sozgen tarafindan gelistirilmeye baslanmistir.
+
+
+#include "BerkushOnlineGM_Base.h"
+
---
+++ Plugins/BerkushOnline/Source/BerkushOnline/Public/BerkushOnlineGM_Base.h
@@ -0,0 +1,17 @@
+// 2024'te R. Berk Sozgen tarafindan gelistirilmeye baslanmistir.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BerkushOnlineGM_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BERKUSHONLINE_API ABerkushOnlineGM_Base : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
